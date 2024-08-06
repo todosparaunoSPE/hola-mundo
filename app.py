c8dbde9d2367deda93aa6c5f8a97275224e05799
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-08-06 11:03:39 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  6 11:01:54 2024
 
+@author: jperezr
+"""
+
+import streamlit as st
+
+st.title('¡Hola, Streamlit!')
+st.write('Esta es una aplicación básica de Streamlit.')
+
+if st.button('Di hola'):
+    st.write('¡Hola, mundo!')
